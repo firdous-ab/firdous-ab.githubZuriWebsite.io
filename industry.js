e641297ae3ef03768ef3a94ffd015f01ad8b11b4
firdous-ab <fridausb2004@gmail.com>
2022-09-08 08:56:22 +0100

    made a few changes and added a JavaScript file

--- industry.js
+++ industry.js
@@ -1,6 +1,19 @@
-const click = document.querySelectorAll(".faq-row");
-const displayText = document.querySelectorAll(".faq-answer");
-
-click[i].addEventListener("click", (e) => {
-  displayText.style.visibility = "visible";
-});
+const faq = document.getElementsByClassName("faq-row");
+// const displayText = document.querySelectorAll(".faq-answer");
+let i;
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    console.log(panel.style.maxHeight, panel.scrollHeight);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      console.log(panel.style.maxHeight);
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+// faq.addEventListener("click", (e) => {
+//   displayText.style.display = "block";
+// });
