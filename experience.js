4b1adbbaa0bded7b6314ac75bcd150a12079106a
firdous-ab <fridausb2004@gmail.com>
2022-09-14 16:22:51 +0100

    most recent commit

--- experience.js
+++ experience.js
@@ -1,13 +1,42 @@
-const button = document.getElementsByClassName("Hire-button");
-const popup = document.getElementsByClassName("popup-wrapper");
+const hiremeButton = document.getElementsByClassName("Hire-button");
+const popup = document.getElementById("popup-wrapper");
 
-let i;
-for (i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", () => {
-    popup.style.display = "flex";
+const JoinPoolButton = document.getElementsByClassName("Join-button");
+const popup2 = document.getElementById("popup2-wrapper");
+
+const links = document.getElementsByClassName("header-links");
+
+// ACTIVE LINKS
+let l;
+for (l = 0; l < links.length; l++) {
+  links[l].addEventListener("click", () => {
+    console.log("hello, I'm active", links);
+    // links[l].classList.add("is-active");
   });
+}
 
+// FIRST POPUP
+let i;
+for (i = 0; i < hiremeButton.length; i++) {
+  hiremeButton[i].addEventListener("click", () => {
+    popup.style.display = "block";
+  });
   popup.addEventListener("click", () => {
-    popup.style.display = "none";
+    popup.style.display === "block"
+      ? (popup.style.display = "none")
+      : (popup.style.display = "block");
+  });
+}
+
+// SECOND POPUP
+let j;
+for (j = 0; j < JoinPoolButton.length; j++) {
+  JoinPoolButton[j].addEventListener("click", () => {
+    popup2.style.display = "block";
+  });
+  popup2.addEventListener("click", () => {
+    popup2.style.display === "block"
+      ? (popup2.style.display = "none")
+      : console.log("Hello, false");
   });
 }
